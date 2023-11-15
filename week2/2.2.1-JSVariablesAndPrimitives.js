f3e9e8d51420e04dfa5ba9cab1dcebd1b74072f6
Antonio Roberts <141260347+evroyr@users.noreply.github.com>
2023-11-14 21:29:58 -0500

    JS Variables and Primitives

---
+++ week2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,22 @@
+// create a constant variable (const)
+const = 22:
+// create a variable that can be reassigned (let)
+let name = Antonio
+// create three variables and assign them values of different data types (=)
+let name = kyle:
+let name = brandon:
+let name = damien:
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(42)
+console.log(helloworld!)
+// change the value referenced by a variable (dynamic typing)
+let dynamic typing =33:
+console.log(dynamic typing)
+// print the type of the variable you just changed (typeof)
+
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+console.log("Hello, my name is " + {antonio} + ", and I am learning " + {javascript} + ". I have been practicing for only " +  {4} + " weeks, so my status as a master is: " + {true or false} + ".");
